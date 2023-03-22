986f6a9e4a0bbb0df902e106b6807e5b805b50d0
NaolGB <naolgbasaye@gmail.com>
2023-03-21 23:27:18 -0400

    login/registration with firebase completed

--- Pocket Book/Pocket Book/Views/Login.swift
+++ Pocket Book/Pocket Book/Views/Login.swift
@@ -1,14 +1,28 @@
 import SwiftUI
+import Firebase
 
 struct Login: View {
     @State private var email: String = ""
     @State private var password: String = ""
     @State private var confirmPassword: String = ""
     @State var registerSheetOpen: Bool = false
+    @State var loginFailed: Bool = false
+    @State private var loginSuccess: Bool = false
+    @State var registerFailedValidation: Bool = false
+    @State var registerFailedError: Bool = false
     
     var body: some View {
+        if loginSuccess {
+            Home()
+        }
+        else{
+            loginView
+        }
+    }
+    
+    var loginView: some View {
         VStack {
-            Text("Login")
+            Text("PocketBook")
                 .font(.largeTitle)
                 .padding([.bottom], 50)
             
@@ -16,10 +30,24 @@ struct Login: View {
                 TextField("Email", text: $email)
                     .textFieldStyle(.roundedBorder)
                     .autocorrectionDisabled()
+                    .autocapitalization(UITextAutocapitalizationType.none)
                 SecureField("Password", text: $password)
                     .textFieldStyle(.roundedBorder)
+                    .padding([.bottom], 25)
+                Button("Login") {
+                    login()
+                }
+                .buttonStyle(.borderedProminent)
+                .disabled(email == "" || password == "")
             }
-                .padding([.bottom], 50)
+            .padding([.bottom], 50)
+            
+            //            login error message
+            if loginFailed {
+                Text("Login failed, email/passowrd incorrect!")
+                    .foregroundColor(Color.red)
+                    .padding([.bottom], 50)
+            }
             
             HStack {
                 Text("New to PocketBook?")
@@ -29,6 +57,8 @@ struct Login: View {
             }
         }
         .padding()
+        
+        //        registration view
         .sheet(isPresented: $registerSheetOpen) {
             VStack {
                 Text("Signup")
@@ -39,6 +69,7 @@ struct Login: View {
                     TextField("Email", text: $email)
                         .textFieldStyle(.roundedBorder)
                         .autocorrectionDisabled()
+                        .autocapitalization(UITextAutocapitalizationType.none)
                     SecureField("Password", text: $password)
                         .textFieldStyle(.roundedBorder)
                     SecureField("Confirm Password", text: $confirmPassword)
@@ -47,9 +78,29 @@ struct Login: View {
                 .padding([.bottom], 50)
                 
                 Button("Signup") {
-                    
+                    if isValidEmail(email) && password == confirmPassword {
+                        register()
+                    }
+                    else {
+                        registerFailedValidation = true
+                    }
                 }
                 .buttonStyle(.borderedProminent)
+                
+                //                registraiton error messages
+                VStack {
+                    if registerFailedValidation {
+                        Text("Email invalid or passwords don't match")
+                            .foregroundColor(Color.red)
+                    }
+                    
+                    if registerFailedError {
+                        Text("Registraiton failed, please try again\nUse a stronger password!")
+                            .foregroundColor(Color.red)
+                            .multilineTextAlignment(.center)
+                    }
+                }
+                .padding()
                 
                 HStack {
                     Text("Already have an account?")
@@ -61,6 +112,41 @@ struct Login: View {
             }
             .padding()
         }
+    }
+    
+    
+    //    Firebase Auth
+    func register() {
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            if error != nil {
+                //                enable static registration failed message
+                registerFailedError = true
+            }
+            else{
+                registerSheetOpen = false
+            }
+        }
+    }
+    
+    func login() {
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+            if error != nil {
+                //                enable static login failed message
+                loginFailed = true
+            }
+            else {
+                loginSuccess = true
+            }
+        }
+    }
+    
+    //    https://stackoverflow.com/questions/25471114/how-to-validate-an-e-mail-address-in-swift
+    //    email validation
+    func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
     }
 }
 
