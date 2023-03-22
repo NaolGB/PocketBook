d4260798d52926e3ec613ea6c51aa0d87889906e
NaolGB <naolgbasaye@gmail.com>
2023-03-21 20:50:19 -0400

    setup Swift app

---
+++ Pocket Book/Pocket Book/Pocket_BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pocket_BookApp.swift
+//  Pocket Book
+//
+//  Created by Naol Basaye on 3/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Pocket_BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
